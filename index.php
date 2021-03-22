a558c1b35ef2d87ba67c27faf876640e37f40fef
Christian <christian544c@hotmail.com>
2021-03-22 14:26:18 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	css/styles.scss

--- index.php
+++ index.php
@@ -9,7 +9,26 @@
 </head>
 
 <body>
-<img src="images/hero.jpeg" class="img-fluid" alt="...">
+    <div class="heroimage">
+        <img src="images/hero.jpeg" class="img-fluid" alt="...">
+    </div>
+
+    <div class="fm">
+        <h1>Formål</h1>
+        <p>Lions organisationen blev oprettet i USA i 1917 på en tid, hvor mange syntes, at verden behøvede mere medieven og mindre selvoptagethed.
+            <br>
+        Organisationen er med til at gøre en forskel over hele verden. På verdensplan er der 1,4 millioner medlemmer fordelt på 45.000 klubber i mere end 200 lande.
+            <br>
+            Lions Danmark donerer årligt omkring 25-30 millioner kroner til humanitære formål. Disse penge fordeles suverænt af den enkelte klub og går til både internationale, nationale og formål i nærområdet.
+            <br>
+        </p>
+    </div>
+
+    <div class="midimage">
+        <img src="images/hero.jpeg" class="img-fluid" alt="...">
+    </div>
+
+    <div class=""></div>
 
 </body>
 </html>
