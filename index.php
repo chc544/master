786d413b03284934638932522f7008467a3f74ab
Christian <christian544c@hotmail.com>
2021-03-22 10:31:29 +0100

    Bootstrap, SASS - init.

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/styles.css" type="text/css">
+
 <html>
 <head>
     <title>Basic Web Page</title>
