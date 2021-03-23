454436c242e4a0b8bc63898d576929fa590b01b3
David <david.osbeck1996@gmail.com>
2021-03-23 10:04:16 +0100

    Kontakt er kodet og gjordt responsiv mobil>tablet>desktop, bootstrap er brugt til cards og containers.

--- includes/footer.php
+++ includes/footer.php
@@ -8,8 +8,8 @@
             <a href="#"><img src="img/LCI_emblem_2color_web.png" alt="logo"></a>
         </div>
             <div class="footer__social">
-                <a href="https://www.facebook.com/Lions-Nyk%C3%B8bing-F-722739517762373/"><img src="img/facebook-square-brands.svg" alt="facebooklogo"></a>
-                <a href="https://www.instagram.com/"><img src="img/instagram-square-brands.svg" alt="instagramlogo"></a>
+                <a href="https://www.facebook.com/Lions-Nyk%C3%B8bing-F-722739517762373/"><img src="img/facebook.svg" alt="facebooklogo"></a>
+                <a href="https://www.linkedin.com/"><img src="img/linkedin.svg" alt="linkedinlogo"></a>
             </div>
             <ul class="footer__list">
                 <li class="footer__list__item"><a href="#">OM OS</a></li>
--- kontakt.php
+++ kontakt.php
@@ -1,1 +1,42 @@
-<?php
+<link rel="stylesheet" href="css/kontakt.scss" type="text/css">
+
+<section class="kontakt">
+    <div class="container">
+        <h1>Kontakt</h1>
+        <h3>Mødested:</h3>
+        <p>Borgerstiftelsen<br>
+        Tværgade<br>
+        4800 Nykøbing F. <br>
+        <br>
+        Klubben mødes: 1. mandag kl 18:00</p>
+
+        <div class="kontakt__email">
+            <img src="img/envelope-regular.svg" alt="envelope">
+            <p>nykoebingfalster@lions.dk</p>
+        </div>
+    </div>
+</section>
+
+<section class="bestyrelse">
+    <h3>Bestyrelse:</h3>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <img src="" alt="ronechortzen">
+                <h4>PRÆSIDENT</h4>
+                <h5>Rone Chortzen</h5>
+                <h5>+45 20 53 11 77</h5>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <img src="" alt="jenshansen">
+                <h4>VICEPRÆSIDENT</h4>
+                <h5>Jens Hansen</h5>
+                <h5>+45 26 78 84 35</h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "includes/footer.php"?>
