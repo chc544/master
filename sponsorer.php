e9d3c914989d9f7a868a365da2adb0e36efc4674
David <david.osbeck1996@gmail.com>
2021-03-23 14:53:24 +0100

    Sponsorer side oprettet, bootstrap er brugt til container. Tilføjet ikoner til medlems siden.

--- sponsorer.php
+++ sponsorer.php
@@ -1,1 +1,24 @@
-<?php
+<link rel="stylesheet" href="css/sponsorer.scss" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+<section class="sponsorer">
+    <div class="container">
+        <h1>Sponsorer</h1>
+        <p>Alle vores sponsorer bliver nævnt i vores Facebookopslag (mm. andet aftales) for eksponering af din virksomhed.<br>
+        Vi har en liste <a href="">her</a> på siden, hvor sponsorerne fremhæves, ligesom de bliver nævnt i vores <a href="">nyhedsbrev</a>.<br><br>
+        Som sponsor for Lions er der flere måder at støtte på.<br>
+        Det kan eskempelvis være:</p>
+        <ul>
+            <li>Gavekort vi kan bruge i vores ltoteri, eller i vores julekurve.</li>
+            <li>Kontantbeløb vi kan bruge til donationer.</li>
+            <li>Gevinster vi kan bruge til vores lotteri.</li>
+        </ul>
+        <p>Vi er meget modtagelige over for andre former for sponsorater der ikke er nævnt – kun fantasien og giveren sætter grænser.<br><br>
+        Når din virksomhed støtter Lions, kan det trækkes fra i skat. Virksomheder kan ubegrænset donere – dog må beløbet højest udgøre 15% af virksomhedens indtjening.
+        <br><br>
+        Læs mere på <a href="https://skat.dk/">skat.dk</a></p>
+    </div>
+</section>
+
+<?php include "includes/footer.php"?>
