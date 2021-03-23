75a31d56004e0d7689ed2ba6d11ac47d1a25fbf4
Christian <christian544c@hotmail.com>
2021-03-23 20:20:09 +0100

    Ændringer til Forside + Menu

--- includes/menu.php
+++ includes/menu.php
@@ -8,23 +8,23 @@
         <div class="col-6 d-flex justify-content-center p-0">
             <div class="social">
                 <a href="">
-                    <img src="img/facebook-square-brands.svg" alt="Lions Nyk F Facebook side" height="25" width="25">
+                    <img src="img/facebook.svg" alt="Lions Nyk F Facebook side" height="25" width="25">
                 </a>
                 <a href="">
-                    <img src="img/instagram-square-brands.svg" alt="Lions Nyk F Linkedin side" height="25" width="25">
+                    <img src="img/linkedin.svg" alt="Lions Nyk F Linkedin side" height="25" width="25">
                 </a>
             </div>
         </div>
 
         <div class="col-6 d-flex justify-content-center p-0">
-            <a href="#" class="btn btn-sm btn-info bg-yellow border border-dark border-2" role="button">Donation</a>
-            <a href="#" class="btn btn-sm btn-info bg-yellow border border-dark border-2" role="button">Blev medlem</a>
+            <a href="#" class="btn btn-sm btn-info bg-yellow border border-dark border-2" id="knap1" role="button">Donation</a>
+            <a href="#" class="btn btn-sm btn-info bg-yellow border border-dark border-2" id="knap1" role="button">Blev medlem</a>
         </div>
 
     </div>
 </div>
-
-<nav class="navbar navbar-expand-lg navbar-light">
+<div class="container">
+<nav class="navbar navbar-right navbar-expand-lg navbar-light">
     <div class="container-fluid p-0">
         <a class="navbar-brand" href="#">
             <img src="img/LCI_emblem_2color_web.png" alt="" width="50" height="50" class="d-inline-block align-top">
@@ -49,30 +49,6 @@
             </ul>
         </div>
         </div>
-</nav>
-
-<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Forside</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="billetter.html">Billetter</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="artister.html">Artister</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="omos.html">Information</a>
-                </li>
-
-            </ul>
-        </div>
     </div>
 </nav>
+</div>
