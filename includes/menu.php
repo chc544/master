fa619df9787f2dc97fbb7da8640842d5829b45b8
ander <soer9428@edu.easj.dk>
2021-03-23 14:33:21 +0100

    menubar opdateret, oprettelse af medlem scss.

--- includes/menu.php
+++ includes/menu.php
@@ -1,18 +1,35 @@
 <head>
     <link rel="stylesheet" href="css/menu.scss" type="text/css">
+
 </head>
 
-<div class="hans">
-    <div class="action__link"></div>
-    <div class="action__btn"></div>
+<div class="container-fluid p-0 bg-primary">
+    <div class="row">
+        <div class="col-6 d-flex justify-content-center p-0">
+            <div class="social">
+                <a href="">
+                    <img src="img/facebook-square-brands.svg" alt="Lions Nyk F Facebook side" height="25" width="25">
+                </a>
+                <a href="">
+                    <img src="img/instagram-square-brands.svg" alt="Lions Nyk F Linkedin side" height="25" width="25">
+                </a>
+            </div>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center p-0">
+            <a href="#" class="btn btn-sm btn-info bg-yellow border border-dark border-2" role="button">Donation</a>
+            <a href="#" class="btn btn-sm btn-info bg-yellow border border-dark border-2" role="button">Blev medlem</a>
+        </div>
+
+    </div>
 </div>
 
-<nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid p-0">
         <a class="navbar-brand" href="#">
             <img src="img/LCI_emblem_2color_web.png" alt="" width="50" height="50" class="d-inline-block align-top">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -29,6 +46,32 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Kontakt</a>
                 </li>
+            </ul>
+        </div>
+        </div>
+</nav>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="billetter.html">Billetter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="artister.html">Artister</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="omos.html">Information</a>
+                </li>
+
             </ul>
         </div>
     </div>
