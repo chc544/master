58a610decf5b4c99ad6c4c8fb77d363a2537a894
David <david.osbeck1996@gmail.com>
2021-03-23 12:57:09 +0100

    Kontakt siden er opdateret med oversigt af medlemmer, hertil er der tilføjet billeder af hvert medlem og af bestyrelsen. Aktiviteter siden er blevet kodet og sat op i bootstrap grid, hertil er der tilføjet ikoner til hver aktivitet.

--- aktiviteter.php
+++ aktiviteter.php
@@ -1,31 +1,68 @@
-<!DOCTYPE html>
-<!-- saved from url=(0086)file:///C:/Users/david/AppData/Local/Temp/Rar.22826/HTML-grundopsaetning.html -->
-<html lang="da"><!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+<link rel="stylesheet" href="css/aktiviteter.scss" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/styles.css" type="text/css">
 
+<section class="aktiviteter">
+    <div class="container">
+        <h1>Aktiviteter</h1>
+    </div>
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="aktiviteter__title__wrapper">
+                    <img src="img/sour-soup.svg" alt="soup">
+                    <h3>Torvesuppe</h3>
+                </div>
+                <p>Hvert år i efterårsferien arrangerer vi i samarbejde med de lokale restauratører “Torvesuppe”. Restauratørerne har hver en stand,
+                    hvor man har mulighed for at smage deres forskellige, lækre supper - En portion suppe koster 30 kr, heraf går 20 kr ubeskåret til koncerten.
+                    Overskuddet fra arrangementet går til at afholde en koncert på torvet i uge 27 i samarbejde med frivillige lokale musikere.
+                    Lions afholder samtidig lotteri og overskuddet fra lotteriet går til et navngivet lokalt formål
+                    Ønsker du at deltage med en stand - klik <a href="">her</a> og kom videre til vores kontaktoplysninger
+                    Ønsker du at sponsorere vores lotteri - klik <a href="">her</a>
+                </p>
+            </div>
 
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
-    <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
-    <meta name="copyright" content="Information om copyright">
+            <div class="col">
+                <div class="aktiviteter__title__wrapper">
+                    <img src="img/smartphone.svg" alt="smartphone">
+                    <h3>Mobil med mening</h3>
+                </div>
+                <p>Vi indsamler brugte, men brugbare mobiltelefoner, der bliver tjekket igennem og videregives til bl.a. børn og hjemløse der mangler.
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="file:///C:/Users/david/AppData/Local/Temp/Rar.22826/css/styles.css" rel="stylesheet" type="text/css">
+                    Vi søger hele tiden frivillige til at hjælpe, så er det noget for dig, så klik <a href="">her</a> og kom videre til vores side for frivillige
+                    Har du en mobil du ønsker at donere, så klik videre <a href="">her</a>
+                </p>
+            </div>
+        </div>
+    </div>
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="aktiviteter__title__wrapper">
+                    <img src="img/heart.svg" alt="heart">
+                    <h3>Uddeling af overskudsmad</h3>
+                </div>
+                <p>Vi indsamler og udleverer overskudsmad fra lokale butikker og udleverer mandag, onsdag og fredag fra vores lokaler i Tværgade - klik <a href="">her</a> og find vores adresse.
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
-<body>
+                    Vi søger hele tiden frivillige til at hjælpe med afhentning og udlevering, så er det noget for dig, så klik <a href="">her</a> og kom videre til vores side for frivillige
+                </p>
+            </div>
 
-<!-- Her skal sidens indhold ligge -->
+            <div class="col">
+                <div class="aktiviteter__title__wrapper">
+                    <img src="img/charity.svg" alt="frivillig">
+                    <h3>Frivillig, hvem, mig?</h3>
+                </div>
+                <p>Vi er i øjeblikket synlige på flere ungdoms- og videregående uddannelser, med vores kampagne “FRIVILLIG, HVEM, MIG?”
+                    Kampagnen henvender sig til de helt unge. Vi ønsker at oplyse om vigtigheden af lokale ildsjæle og hvad det giver DIG igen at være frivillig.
+                    Du kan se vores plakater hænge rundt omkring og der hvor de hænger er det også muligt at få udleveret flyers, hvor du kan læse mere
+                    Hent vores plakat og flyer <a href="">her</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
-
-</body>
-</html>
+<?php include "includes/footer.php"?>
