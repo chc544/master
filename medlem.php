55307ec242b4f7c64d75c5f3c58e7ee4fd902880
ander <soer9428@edu.easj.dk>
2021-03-24 09:54:43 +0100

    Små ændringer til siden "Blev medlem" størrelse på container.

--- medlem.php
+++ medlem.php
@@ -21,21 +21,21 @@
 
                     <div class="text d-flex py-4">
                         <img class="me-3" src="img/support.svg" alt="fællesskab" height="30" width="30">
-                        <p>Ved frivilligt arbejde har du mulighed for at lære nye mennesker at kende og indgå i nye fællesskaber</p>
+                        <p>Med frivilligt arbejde har du mulighed for at lære nye mennesker at kende og indgå i nye fællesskaber.</p>
                     </div>
 
                     <div class="text d-flex py-4">
                         <img class="me-3" src="img/handsholdingheart.svg" alt="fællesskab">
-                        <p>Du kan komme helt tæt på og opleve, at du gør en forskel for andre mennesker gennem dit frivillige arbejde</p>
+                        <p>Du ser med egne øjne, den forskel du gør for andre mennesker gennem dit frivillige arbejde.</p>
                     </div>
 
                     <div class="text d-flex py-4">
                         <img class="me-3" src="img/book.svg" alt="fællesskab">
-                        <p>Du kan bruge eksisterende færdigheder og få ny viden, erfaring og kompetencer</p>
+                        <p>Bliv bedre til det du allerede kan, få ny viden, erfaring og kompetencer.</p>
                     </div>
 
                     <div class="d-flex justify-content-center pt-3 pb-5">
-                        <a class="btn btn-yellow" href="#" role="button">Bliv frivillig</a>
+                        <a class="btn btn-yellow" href="#" role="button">Bliv Frivillig</a>
                     </div>
 
         </div>
@@ -53,19 +53,19 @@
 
                     <div class="text d-flex py-4">
                         <img class="me-3" src="img/heart.svg" alt="fællesskab" height="30" width="30">
-                        <p>Med en månedlig betaling er du med til at hjælpe de sårbare i lokalsamfundet</p>
+                        <p>For under 3,5 kr om dagen, er du med til at hjælpe andre i nød.</p>
 
                     </div>
 
                     <div class="text d-flex py-4">
                         <img class="me-3" src="img/love.svg" alt="fællesskab">
-                        <p>Det kan give ro på sindet at dine penge går til et godt formål</p>
+                        <p>Det giver ro i sindet at vide, dine penge går til et godt formål.</p>
 
                     </div>
 
                     <div class="text d-flex py-4">
                         <img class="me-3" src="img/community.svg" alt="fællesskab">
-                        <p>Du kan blive en del af et større fællesskab der er med til at gøre en forskel</p>
+                        <p>Du bliver en del af et større lokalt fællesskab der er med til at gøre en forskel.</p>
 
                     </div>
 
